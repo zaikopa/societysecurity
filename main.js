399aea9c0aad0cea29a0d74fdb9ac9bdba680162
zaikopa <lucianocabral2008@gmail.com>
2025-05-11 23:05:24 -0300

    Se agrego un mensaje al main

---
+++ main.js
@@ -0,0 +1,9 @@
+ScrollReveal().reveal('.seccion-dos-columnas', {
+  duration: 1000,
+  distance: '30px',
+  origin: 'bottom',
+  easing: 'ease-out',
+  reset: false,
+  mobile: true,
+  delay: 200
+});
